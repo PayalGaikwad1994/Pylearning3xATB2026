cd7efb1f291643a984fc1c896a08d14cdcacc671
Payal Gaikwad <poyal1994@gmail.com>
2026-03-23 01:07:01 +0530

    adding new files

---
+++ Ex_21_032026/Lab019.py
@@ -0,0 +1,7 @@
+# Ternary Operator
+
+# if condition then do this
+#     else do that
+
+age=int(input("Enter your age: "))
+print("I will go to Goa" if age>18 else "I am not allowed go to Goa")
---
+++ Ex_21_032026/Lab020.py
@@ -0,0 +1,6 @@
+#calculate area of circle
+
+pi=3.14
+r=float(input("Enter a radius of circle:"))
+
+print('Area of circle is {}'.format(pi*r**2))
